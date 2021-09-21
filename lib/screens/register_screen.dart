de3c7bfae7488eca73d3ac77bbef181a99ca6e6b
husseinhesham24 <husseinhesham24@gmail.com>
2021-09-22 01:29:29 +0200

    fix and update registration screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -9,35 +9,6 @@ import 'package:http/http.dart' as http;
 
 dynamic data;
 
-Future<void> createAlbum(String name, String email, String password,
-    String passwordConfirmation, String phoneNumber, String dateOfBirth) async {
-  final response = await http.post(
-    Uri.parse('https://inline.mrtechnawy.com/api/auth/register'),
-    headers: <String, String>{
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: jsonEncode(<String, String>{
-      'name': name,
-      'email': email,
-      'password': password,
-      'password_confirmation': passwordConfirmation,
-      'phone_number': phoneNumber,
-      'date_of_birth': dateOfBirth,
-    }),
-  );
-
-  print(response.body);
-
-  if (response.statusCode == 201) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    data = jsonDecode(response.body);
-  } else {
-    data = jsonDecode(response.body);
-  }
-}
-
 class Register_Screen extends StatefulWidget {
   @override
   _Register_ScreenState createState() => _Register_ScreenState();
@@ -52,11 +23,62 @@ class _Register_ScreenState extends State<Register_Screen> {
   TextEditingController _dateOfBirth = TextEditingController();
   dynamic _data = null;
 
-  void _startAction(BuildContext ctx) {
-    createAlbum(_name.text, _email.text, _password.text,
-        _passwordConfirmation.text, _phoneNumber.text, _dateOfBirth.text);
+  Future<void> createAlbum(
+    String name,
+    String email,
+    String password,
+    String passwordConfirmation,
+    String phoneNumber,
+    String dateOfBirth,
+    BuildContext ctx,
+  ) async {
+    final response = await http.post(
+      Uri.parse('https://inline.mrtechnawy.com/api/auth/register'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: jsonEncode(<String, String>{
+        'name': name,
+        'email': email,
+        'password': password,
+        'password_confirmation': passwordConfirmation,
+        'phone_number': phoneNumber,
+        'date_of_birth': dateOfBirth,
+      }),
+    );
+
+    print(response.body);
+
+    if (response.statusCode == 201) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      data = jsonDecode(response.body);
+    } else {
+      data = jsonDecode(response.body);
+    }
+
+    _verfyReg(ctx);
+  }
+
+  void _sendRequest(BuildContext ctx) {
+    createAlbum(
+      _name.text,
+      _email.text,
+      _password.text,
+      _passwordConfirmation.text,
+      _phoneNumber.text,
+      _dateOfBirth.text,
+      ctx,
+    );
+  }
+
+  void _verfyReg(BuildContext ctx) {
+    print(_data);
+    print("lol");
     setState(() {
       _data = data;
+      print(_data['status']);
       if (_data['status']) {
         Navigator.of(ctx).push(
           MaterialPageRoute(
@@ -68,8 +90,6 @@ class _Register_ScreenState extends State<Register_Screen> {
       }
     });
   }
-
-   
 
   void _navToBack(BuildContext ctx) {
     Navigator.of(ctx).push(
@@ -199,7 +219,7 @@ class _Register_ScreenState extends State<Register_Screen> {
             SizedBox(
               height: 100,
             ),
-            Button_Widget("Start", _startAction),
+            Button_Widget("Start", _sendRequest),
           ],
         ),
       ),
